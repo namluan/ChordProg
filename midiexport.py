6475e21e12513936469daa81eb1f531a98925a67
namluan <nam.guerin@gmail.com>
2025-05-09 01:01:05 +0200

    export midi added

--- midiexport.py
+++ midiexport.py
@@ -5,5 +5,11 @@ def export_midi(progression, filename="test_prog"):
     midi = MIDIFile(1)
     track = 0
     time = 0
-
-
+    midi.addTrackName(track, time, "Chord Track")
+    midi.addTempo(track, time, 130)
+    for chord_obj in progression:
+        for pitch in chord_obj.pitches:
+            midi.addNote(track, 0, pitch.midi, time, 1, 100)
+        time += 1
+    with open(filename, "wb") as output_file:
+        midi.writeFile(output_file)
